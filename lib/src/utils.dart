b4e5d727be190f39cb0578cca9e15669a8c9a7cb
Caleb Marshall <caleb.marshall108@gmail.com>
2023-09-09 19:37:46 -0400

    lib/src/utils: added back flutter deprecated method "DecoderCallback"

--- lib/src/utils.dart
+++ lib/src/utils.dart
@@ -454,3 +454,5 @@ class OffsetTween extends Tween<Offset?> {
   @override
   Offset lerp(double t) => (begin! + (end! - begin!) * t);
 }
+
+typedef DecoderCallback = Future<ui.Codec> Function(Uint8List buffer, {int? cacheWidth, int? cacheHeight, bool allowUpscaling});
